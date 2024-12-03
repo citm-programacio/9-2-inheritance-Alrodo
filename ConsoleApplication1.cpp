2ce4179b5f5b1bf629ccf9027697c7b225e177e8
Alrodo <albert.rodriguez.domingo@estudiantat.upc.edu>
2024-12-03 19:18:24 +0100

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,10 +2,66 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Building {
+protected:
+    string name;
+
+public:
+    Building(string buildingNom) : name(buildingNom) {}
+    string getName() {
+        return name;
+    }
+};
+class Warehouse : public Building {
+private:
+    int wood, rocks, wheat;
+public:
+    Warehouse(string warehouseNom, int fusta, int roques, int trigo) : Building(warehouseNom), wood(fusta), rocks(roques), wheat(trigo) {}
+    void printResources() {
+        cout << "Nom del magatzem: " << name << endl;
+        cout << "Unitats de fusta: " << wood << endl;
+        cout << "Unitats de roca: " << rocks << endl;
+        cout << "Unitats de trigo: " << wheat << endl;
+    }
+};
+class House : public Building {
+private:
+    int floors, inhabitants, servants;
+public:
+    House(string houseNom, int plantes, int habitants, int servents) : Building(houseNom), floors(plantes), inhabitants(habitants), servants(servents) {}
+    void printHouse() {
+        cout << "Nom de la casa: " << name << endl;
+        cout << "Numero de plantes: " << floors << endl;
+        cout << "Numero d'habitants: " << inhabitants << endl;
+        cout << "Numero de servents: " << servants << endl;
+    }
+};
+class Temple : public Building {
+private:
+    string god;
+    int priests;
+public:
+    Temple(string houseNom, string deu, int apostols) : Building(houseNom), god(deu), priests(apostols) {}
+    void printTemple() {
+        cout << "Nom de la casa: " << name << endl;
+        cout << "Nom del Deu: " << god << endl;
+        cout << "Numero d'apostols: " << priests << endl;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Warehouse warehouse("the warehouse", 21, 34, 56);
+    House house("Casablanca", 2, 6, 3);
+    Temple temple("Esglesia", "Joselu", 38);
+    warehouse.printResources();
+    cout << endl;
+    house.printHouse();
+    cout << endl;
+    temple.printTemple();
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
